877367ca1c8f07b71ace5dea2deac0300ccfc7dd
meXuvo <cse.engr.shuvo@gmail.com>
2021-02-11 20:56:24 +0600

    fullCalender customisation

---
+++ public/js/activeFullCalendar.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        dateClick: function() {
+            const koModal = document.querySelector('#ko-modal');
+            koModal.classList.remove("hidden");
+            koModal.classList.add("flex");
+            window.onclick = function(event) {
+                if (event.target == koModal) {
+                  koModal.classList.remove("flex");
+                  koModal.classList.add("hidden");
+                }
+              }
+              document.addEventListener("scroll",(event)=>{
+                koModal.classList.remove("flex");
+                koModal.classList.add("hidden");
+              })
+          },
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+      },
+      initialDate: '2020-09-12',
+      navLinks: true, // can click day/week names to navigate views
+      businessHours: true, // display business hours
+      editable: true,
+      selectable: true,
+      events: [
+        {
+          title: 'Business Lunch',
+          start: '2020-09-03T13:00:00',
+          constraint: 'businessHours'
+        },
+        {
+          title: 'Meeting',
+          start: '2020-09-13T11:00:00',
+          constraint: 'availableForMeeting', // defined below
+          color: '#257e4a'
+        },
+        {
+          title: 'Conference',
+          start: '2020-09-18',
+          end: '2020-09-20'
+        },
+        {
+          title: 'Party',
+          start: '2020-09-29T20:00:00'
+        },
+
+        // areas where "Meeting" must be dropped
+        {
+          groupId: 'availableForMeeting',
+          start: '2020-09-11T10:00:00',
+          end: '2020-09-11T16:00:00',
+          display: 'background'
+        },
+        {
+          groupId: 'availableForMeeting',
+          start: '2020-09-13T10:00:00',
+          end: '2020-09-13T16:00:00',
+          display: 'background'
+        },
+
+        // red areas where no events can be dropped
+        {
+          start: '2020-09-24',
+          end: '2020-09-28',
+          overlap: false,
+          display: 'background',
+          color: '#ff9f89'
+        },
+        {
+          start: '2020-09-06',
+          end: '2020-09-08',
+          overlap: false,
+          display: 'background',
+          color: '#ff9f89'
+        }
+      ],
+    });
+
+    calendar.render();
+  });
